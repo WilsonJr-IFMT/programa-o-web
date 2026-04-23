a50c6c172553b33e2276bafdb869e12f790e4516
WilsonJr-IFMT <w.giachini@estudante.ifmt.edu.br>
2026-04-22 20:32:13 -0400

    Exercício1

---
+++ Exercicio1.js
@@ -0,0 +1,10 @@
+let nome = "Wilson"
+let idade =  38
+let valorlogico = true 
+
+
+console.log("Eu" +" "+ nome +" " + "tenho" + " " + idade + " " +"anos."+" "+"É"+" "+ valorlogico);
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof valorlogico)
+
